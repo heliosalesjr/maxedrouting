13bee71cb0cb583787cdac6d9f04b824b53144b5
heliosalesjr <heliosalesjr@gmail.com>
2024-05-14 18:02:56 -0300

    404 pages

---
+++ app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFoundPage() {
+  return (
+    <>
+        <h1 className='text-center items-center text-5xl'>Hey! Not found 404!</h1>
+        <p className='text-center items-center text-2xl p-8'>Now try to type the right thing!</p>
+    </>  
+)
+}
+
+export default NotFoundPage
